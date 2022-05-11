82f1691e1882a64db7c0731f70fabb7f56df4d60
Sav0112 <74131851+Sav0112@users.noreply.github.com>
2022-05-11 15:37:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
-song1 = "";
-song2 = "";
-
-song1_status = "";
-song2_status = "";
-
-scoreRightWrist = 0;
+song = "";
 scoreLeftWrist = 0;
+scoreRightWrist = 0;
 
 rightWristX = 0;
 rightWristY = 0;
@@ -13,10 +8,8 @@ rightWristY = 0;
 leftWristX = 0;
 leftWristY = 0;
 
-function preload()
-{
-    song1 = loadSound("music.mp3");
-    song2 = loadSound("music2.mp3");
+function preload() {
+    song = loadSound("music.mp3");
 }
 
 function setup() {
@@ -34,65 +27,70 @@ function modelLoaded() {
     console.log('PoseNet Is Initialized');
 }
 
-function gotPoses(results)
-{
+function gotPoses(results) {
     if(results.length > 0)
     {
         console.log(results);
-        scoreRightWrist = results[0].pose.keypoints[10].score;
         scoreLeftWrist = results[0].pose.keypoints[9].score;
-        console.log("scoreRightWrist = " + scoreRightWrist + "scoreLeftWrist = " + scoreLeftWrist);
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
+        console.log("scoreRightWrist = " + scoreRightWrist);
 
         rightWristX = results[0].pose.rightWrist.x;
         rightWristY = results[0].pose.rightWrist.y;
-        console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
+        console.log("Right Wrist X = " + rightWristX + "Right Wrist Y = " + rightWristY);
 
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
-        console.log("lefttWristX = " + leftWristX +" lefttWristY = "+ leftWristY);
-
+        console.log("Left Wrist X = " + leftWristX + "Left Wrist Y = " + leftWristY);
     }
 }
 
 function draw() {
     image(video, 0, 0, 600, 500);
 
-    song1_status = song1.isPlaying();
-    song2_status = song2.isPlaying();
-
     fill("#FF0000");
     stroke("#FF0000");
 
-    if(scoreRightWrist > 0.2)
+    circle(rightWristX, rightWristY, 20);
+
+    if(rightWristY > 0 && rightWristY <= 100)
     {
-        circle(rightWristX, rightWristY, 20);
-
-            song2.stop();
-
-        if(song1_status == false)
-        {
-            song1.play();
-            document.getElementById("song").innerHTML = "Playing- Evermore"
-        }
+        document.getElementById("speed").innerHTML = "Speed = 0.5x";
+        song.rate(0.5);
+    }
+    else if(rightWristY > 100 && rightWristY <= 200)
+    {
+        document.getElementById("speed").innerHTML = "Speed = 1x";
+        song.rate(1);
+    }
+    else if(rightWristY > 200 && rightWristY <= 300)
+    {
+        document.getElementById("speed").innerHTML = "Speed = 1.5x";
+        song.rate(1.5);
+    }
+    else if(rightWristY > 300 && rightWristY <= 400)
+    {
+        document.getElementById("speed").innerHTML = "Speed = 2x";
+        song.rate(2);
+    }
+    else if(rightWristY > 400 && rightWristY <= 500)
+    {
+        document.getElementById("speed").innerHTML = "Speed = 2x";
+        song.rate(2);
     }
 
     if(scoreLeftWrist > 0.2)
     {
-        circle(leftWristX, leftWristY, 20);
-
-            song1.stop();
-
-        if(song2_status == false)
-        {
-            song2.play();
-            document.getElementById("song").innerHTML = "Playing- Love Story"
-        }
+    circle(leftWristX, leftWristY, 20);
+    InNumberleftWristY = Number(leftWristY);
+    remove_decimals = floor(InNumberleftWristY);
+    volume = remove_decimals/500;
+    document.getElementById("volume").innerHTML = "Volume = " + volume;
+    song.setVolume(volume);
     }
-
 }
-
-function play()
-{
+function play() {
     song.play();
     song.setVolume(1);
     song.rate(1);
